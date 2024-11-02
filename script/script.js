0bae82f7a57a1a99b4482488a8c27ec6f38f36bf
岩熊 <153962290+ikasumi123@users.noreply.github.com>
2024-11-02 20:11:05 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,18 +7,3 @@ function password() {
     alert("パスワードが違います！");
     }
 }
-$(function(){
-    var pagetop = $('#page-top');
-    pagetop.hide();
-    $(window).scroll(function () {
-       if ($(this).scrollTop() > 100) {
-            pagetop.fadeIn();
-       } else {
-            pagetop.fadeOut();
-       }
-    });
-    pagetop.click(function () {
-       $('body, html').animate({ scrollTop: 0 }, 500);
-       return false;
-    });
-  });
