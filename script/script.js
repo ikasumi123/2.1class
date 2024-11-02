68db5d27654073b7d32ba1e280f6c17df8c2098b
岩熊 <153962290+ikasumi123@users.noreply.github.com>
2024-11-02 10:00:53 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function password() {
+    var pw;
+    pw = prompt("パスワードを入れて下さい。","");
+    if (pw =='2.1-game'){
+        location.href = "2.1class.html";
+    } else {
+    alert("パスワードが違います！");
+    }
+}
