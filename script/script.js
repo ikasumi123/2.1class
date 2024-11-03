dee0f23f151a89673f5785f5660d57eae315944d
岩熊 <153962290+ikasumi123@users.noreply.github.com>
2024-11-03 09:04:54 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 function password() {
-    var pw;
-    pw = prompt("パスワードを入れて下さい。","");
-    if (pw =='2.1-game'){
-        location.href = "2.1class.html";
-    } else {
-    alert("パスワードが違います！");
+    while (pw != '2.1-game'){
+        var pw;
+        pw = prompt("パスワードを入れて下さい。","");
+        if (pw =='2.1-game'){
+            location.href = "2.1class.html";
+        } else {
+        alert("パスワードが違います！");
+        }
     }
 }
