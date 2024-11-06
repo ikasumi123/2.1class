5841f9956b41f5e597cbc8a02de2ca706c49bc3e
岩熊 <153962290+ikasumi123@users.noreply.github.com>
2024-11-07 07:23:06 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,16 +1,23 @@
 function password() {
-    while (pw != '2.1-game'){
-        var pw;
-        pw = prompt("パスワードを入れて下さい。","");
-        if (pw =='2.1-game'){
-            location.href = "2.1class.html";
-        } else {
-        alert("パスワードが違います！");
+    var pw1;
+    var pw2;
+    
+    // ユーザー名の入力を繰り返し求める
+    do {
+        pw1 = prompt("ユーザー名を入力してください", "");
+        if (pw1 != 'ikasumi') {
+            alert('登録されていないユーザーです');
         }
-    }
+    } while (pw1 != 'ikasumi');
+    
+    // ユーザー名が正しければ、パスワードの入力を繰り返し求める
+    do {
+        pw2 = prompt("パスワードを入れて下さい。", "");
+        if (pw2 != '2.1-game') {
+            alert("パスワードが違います！");
+        }
+    } while (pw2 != '2.1-game');
+    
+    // 正しいユーザー名とパスワードが入力された場合、ページ遷移
+    location.href = "2.1class.html";
 }
-fetch('https://api.ipify.org?format=json')
-.then(response => response.json()) // APIのレスポンスをJSONとして解析
-.then(data => { // 解析されたデータを受け取る
-  console.log("Your IP address is: " + data.ip); // IPアドレスをコンソールに表示
-});
